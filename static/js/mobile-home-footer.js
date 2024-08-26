a97f14f23f1188040efcb7da4287df596469770c
hamandy2000 <minji9306@gmail.com>
2024-08-26 21:24:00 +0900

    init

--- static/js/mobile-home-footer.js
+++ static/js/mobile-home-footer.js
@@ -2,11 +2,16 @@
 // div에 스타일이 style="display:none에서 눌렀을 때 style을 없애주면 됨" 해주면 됨.
 // HTMLCollection.prototype.forEach = Array.prototype.forEach;
 const openButton = document.querySelector(".btn-company");
+const iconUp = document.querySelector(".ico-down");
+const iconTest = document.querySelector(".icon-test");
 const detail = document.getElementsByClassName("detail");
 
-// console.log(openButtons);
-// openButtons.forEach((openButton) => {
-openButton.addEventListener("click", (e) => {
-    detail.style.display = "";
+iconUp.addEventListener("click", (e) => {
+    console.log("들어옴");
+    iconUp.style.transform = "rotate(-180deg)";
+    iconUp.style.transition = "transform 0.5s ease";
 });
+
+// iconTest.addEventListener("click", (e) => {
+//     iconTest.style.transform = "rotate(-180deg";
 // });
