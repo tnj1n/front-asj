e907090879221c52df325d141c696f082b5578cf
hamandy2000 <minji9306@gmail.com>
2024-08-27 10:21:52 +0900

    init

--- static/js/mobile-home-footer.js
+++ static/js/mobile-home-footer.js
@@ -1,13 +1,24 @@
 // detail div를 숨겼다가 나타나게 해야됨
 // div에 스타일이 style="display:none에서 눌렀을 때 style을 없애주면 됨" 해주면 됨.
 // HTMLCollection.prototype.forEach = Array.prototype.forEach;
-const openButton = document.querySelector(".btn-company");
+const openButton = document.querySelector(".btn-company.close");
 const iconUp = document.querySelector(".ico-down");
 const iconTest = document.querySelector(".icon-test");
 const detail = document.querySelector(".detail");
 
 iconUp.addEventListener("click", (e) => {
-    iconUp.style.transform = "rotate(-180deg)";
-    iconUp.style.transition = "transform 0.5s ease";
-    detail.style.display = "";
+    if (e.target.classList.cotains(".btn-company.close")) {
+        iconUp.classList.toggle(".btn-company");
+        iconUp.style.transform = "rotate(-180deg)";
+    }
+    // iconUp.style.transform = "rotate(-180deg)";
+    // iconUp.style.transition = "transform 0.5s ease";
+    // detail.style.display = "";
 });
+
+// const mains = document.querySelectorAll("div.main");
+// mains.forEach((main, i) => {
+//     main.addEventListener("click", (e) => {
+//         if (e.target.classList.contains("block")) {
+//             main.classList.toggle("on");
+//         }
