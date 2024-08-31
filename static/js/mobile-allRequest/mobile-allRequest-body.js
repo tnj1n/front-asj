de088050edd50cb967300c8679226d4110dd4175
hamandy2000 <minji9306@gmail.com>
2024-08-31 16:56:06 +0900

    init

--- static/js/mobile-allRequest/mobile-allRequest-body.js
+++ static/js/mobile-allRequest/mobile-allRequest-body.js
@@ -1,13 +1,19 @@
-const selectDivs = document.querySelectorAll(".select-type2");
-const selectArrow1 = document.querySelector(".nice-select:after");
-console.log(selectButtons);
-console.log(selectArrow1);
-// class="nice-select selected open"
+const selectDivs = document.querySelectorAll(".nice-select");
+const selectList = document.querySelectorAll(".list > li");
+console.log(selectDivs);
+console.log(selectList);
 
+// 지역 선택 div 선택
 selectDivs.forEach((selectDiv) => {
     selectDiv.addEventListener("click", (e) => {
-        if (e.target.lastElementChild.value === "div.nice-select.open") {
-            e.target.lastElementChild.remove("open");
+        const target = e.target;
+        // 'open' 클래스가 있는지 확인
+        if (target.classList.contains("open")) {
+            // 클래스가 있으면 제거
+            target.classList.remove("open");
+        } else {
+            // 클래스가 없으면 추가
+            target.classList.add("open");
         }
     });
 });
