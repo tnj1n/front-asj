1a3a1edf0dfd3524dc0ce1723909e4fb980ca979
hamandy2000 <minji9306@gmail.com>
2024-09-02 02:13:31 +0900

    init

--- static/js/mobile-myRequest/mobile-myRequest-body.js
+++ static/js/mobile-myRequest/mobile-myRequest-body.js
@@ -1,3 +1,13 @@
-// const selectButtons = document.querySelectorAll(".tab-type");
-// console.log(selectButtons);
-console.log("들어옴");
+const selectButtons = document.querySelectorAll(".tab-type li");
+console.log(selectButtons);
+
+selectButtons.forEach((selectButton) => {
+    selectButton.addEventListener("click", (e) => {
+        if (e.target.parentElement.classList.contains("active")) {
+            e.target.parentElement.classList.remove("active");
+        } else {
+            e.target.parentElement.classList.add("active");
+        }
+        console.log(e.target);
+    });
+});
